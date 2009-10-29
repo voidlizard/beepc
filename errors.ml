b843553f6f94e00b05a028e0238389909f05cbd6
Dmitry Zuikov <dmz@x200.(none)>
2009-10-29 13:01:11 +0300

    fix

--- errors.ml
+++ errors.ml
@@ -10,7 +10,7 @@ let string_of_error x = match x with
     | CompilerError(TypeError(s), c)       -> sprintf "*** Error [Typing] %s at %s" s (str_of_ctx c)
     | CompilerError(NameError(s), c)       -> sprintf "*** Error [Name] %s at %s" s (str_of_ctx c)
     | CompilerError(OtherError(s), c)      -> sprintf "*** Error [Other] %s at %s" s (str_of_ctx c)
-    | CompilerError(ArgNumError, c)        -> sprintf "*** Error [Wrong number of args in function call] %s" (str_of_ctx c)
+    | CompilerError(ArgNumError, c)        -> sprintf "*** Error [Wrong number of args in function call] at %s" (str_of_ctx c)
     | CompilerError(NameHideError(s,s'),c) -> sprintf "*** Error [Name] Declaring of '%s' hides '%s' at %s" s s' (str_of_ctx c)
     | x                                    -> raise x 
 
